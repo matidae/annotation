00868eeec537e81104b5c674ffefb44709372a1b
matias <matidae@gmail.com>
2016-03-01 00:02:07 -0300

    mod

--- xml_merger.py
+++ xml_merger.py
@@ -2,30 +2,25 @@ import sys
 import xml.etree.cElementTree as et
 import xml.dom.minidom
 
-rooot=""
+root=""
 def merge(lista, blast, annot):
     root =  et.Element("annotation")
     parse_blast = et.parse(blast).getroot()
     parse_annot = et.parse(annot).getroot()
-
-    for i in lista:
-        name = i.strip()
-        blast_node = ""
-        annot_node_list = []
-        for node in parse_blast:
-            if node.attrib["id"] == name:
-                #blast_node == node    
-                blast_node = root.subElement(node)
-                for node2 in parse_annot:
-                    if node2.attrib["id"] == name:
-                        #annot_node_list == [subnode for subnode in node.getchildren()]
-                        print node
-        
+    for pb in parse_blast:
+       name = pb.attrib["id"]
+       for pa in parse_annot:
+                    if pa.attrib["id"] == name:
+                        annot_node_list = [subnode for subnode in pa.getchildren()]
+                        for j in annot_node_list:
+                            pb.append(j)
+       root.append(pb)
+    return root
         
 if __name__ == "__main__":
     try:
         with open(sys.argv[1]) as lista, open(sys.argv[2]) as blast, open(sys.argv[3]) as annot:
-            merge(lista, blast, annot)
+            root = merge(lista, blast, annot)
             stringnewroot = et.tostring(root)
             parsenewroot = xml.dom.minidom.parseString(stringnewroot)
             print parsenewroot.toprettyxml()
