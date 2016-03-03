79dfed0a407cb521a0ca77d6f0bc5d981bdd87a2
matias <matidae@gmail.com>
2016-03-03 16:07:09 -0300

    fix blast

--- annotation_parser.py
+++ annotation_parser.py
@@ -7,6 +7,13 @@ class CDS():
         self.name = name
         self.matches = matches
 
+class Blast():
+    def __init__(self, matchType, desc, evalue, sp):
+        self.matchType = matchType
+        self.desc = desc
+        self.evalue = evalue
+        self.sp = sp
+
 class Entry():
     def  __init__(self, matchType, idn=None, name=None, desc=None, evalue=None, count=None):
         self.matchType = matchType
@@ -24,7 +31,9 @@ def loadXML(xml):
         cds = CDS(name)
         cds_matches = []
         for sub in node:
-            if sub.tag in ["fingerprints", "hmmer3", "hmmer2", "panther", "superfamilyhmmer3"]:
+            if "blast_hit" in sub.tag:
+                entry = Blast(sub.tag, desc=sub.attrib["desc"], evalue=sub.attrib["evalue"], sp=sub.attrib["sp"])
+            elif sub.tag in ["fingerprints", "hmmer3", "hmmer2", "panther", "superfamilyhmmer3"]:
                 entry = Entry(sub.tag, idn=sub.attrib["id"], desc=sub.attrib["desc"], evalue=sub.attrib["evalue"])
             elif sub.tag in ["profilescan", "patternscan"]:
                 entry = Entry(matchType=sub.tag, idn=sub.attrib["id"], desc=sub.attrib["desc"])
@@ -57,13 +66,23 @@ def parse_args():
 def main(cds_all):
     if parse_args().db:
         match_list = parse_args().db.split(",")
-    dbs =["fingerprints", "hmmer3", "hmmer2", "panther", "superfamilyhmmer3","profilescan", "patternscan","signalp", "phobius","coils", "tmhmm","blastprodom","goMF", "goCC", "goBP"]
+    dbs = ["fingerprints", "hmmer3", "hmmer2", "panther", "superfamilyhmmer3","profilescan", "patternscan","signalp", "phobius", "tmhmm","blastprodom","goMF", "goCC", "goBP", "kegg", "unipathway", "reactome", "metacyc", "coils"]
     for cds in cds_all:
         name = cds.name
         line = ""
         if parse_args().all:
             for entry in cds.matches:
-                line += entry.matchType + ":" +entry.idn + ":" + entry.desc.replace(" ","_") + "; "
+                if entry.matchType in dbs:
+                    if entry.desc:
+                        line += entry.matchType + ":" + entry.idn + ":" + entry.desc.replace(" ","_") + "; "
+                    elif entry.evalue:
+                        line += entry.matchType + ":" + entry.desc.replace(" ","_") + ":" + entry.evalue + ":" + entry.sp.replace(" ","_") + "; "
+                    elif entry.count:
+                        line += entry.matchType + ":" + entry.count + "; "
+                    else:
+                        line += entry.matchType + ":" + entry.idn + "; "
+                elif "blast_hit" in entry.matchType:
+                    line += entry.matchType + ":" + entry.desc.replace(" ","_") + ":" + entry.evalue + ":" + entry.sp.replace(" ","_") + "; "
         elif len(match_list) > 0:
             for i in match_list:
                 if i not in dbs:
